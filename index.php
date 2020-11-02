a76d9e02bd17e87f4381937c299ab9c4bdf71276
Rone Bazilio <ronebsantos@gmail.com>
2020-11-02 01:21:14 -0300

    primeiro programa

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
-<?
-
-echo "Roneeee";
 
 
+<form method="POST" enctype="multipart/form-data">
+
+	<input type="file" name="fileUpload">
+
+	<button type="submit">Send</button>
+
+</form>
+
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+
+	$file = $_FILES["fileUpload"];
+
+	if ($file["error"]){
+		throw new Exception("Error: ".$file["error"]);
+
+	}
+
+
+$dirUploads = "uploads";
+
+/*
+if (!is_dir($dirUploads)){
+
+	mkdir ($dirUploads);
+}
+*/
+
+if (move_uploaded_file($file["tmp_name"], $dirUploads.$file["name"])) {
+
+	echo "Upload realizado com sucesso";
+
+	$file["tmp_name"]
+}
+	else{
+
+		throw new Exception("Não foi possivel realizar o upload.");
+	}
+
+}
 ?>
+
